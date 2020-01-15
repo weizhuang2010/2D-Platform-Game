f6d8874390e03bf828f27042651dcf915c65f43b
Wei Zhuang <37267165+weizhuang2010@users.noreply.github.com>
2020-01-15 18:07:13 +0800

    整理代码，GroundState添加了在Shelf上的子状态，把按下每个按钮都单独写成了方法

--- Assets/04.Characters/Scripts/CharacterController2D.cs
+++ Assets/04.Characters/Scripts/CharacterController2D.cs
@@ -1,272 +1,336 @@
-﻿// 2020-01-08 00:56 添加角色动画：闲置、跑，播放时有Bug
+// 2020-01-08 00:56 添加角色动画：闲置、跑，播放时有Bug
 // 2020-01-10 00:12 解决了部分动画切换不正确的问题，仍需继续检查
 // 2020-01-12 23:12 解决了从墙面到地面的动画显示错误的问题
+// 2020-01-15 18:03 整理代码，GroundState添加了在Shelf上的子状态，把按下每个按钮都单独写成了方法
 
 using System;
+using System.Collections;
 using UnityEngine;
 
 public class CharacterController2D : MonoBehaviour
 {
-    public enum Location
-    {
-        Ground,
-        Wall,
-        Shelf,
-        Air
-    }
-
-    [SerializeField] private Transform body;
-    private Rigidbody2D rigidbody2D;
+    #region
+    [SerializeField] private Transform bodyTransform;
+    private new Rigidbody2D rigidbody2D;
     private Animator animator;
 
     [SerializeField] private Transform groundCheck;
-    [SerializeField] private Transform wallCheckL;
-    [SerializeField] private Transform wallCheckR;
+    [SerializeField] private Transform lowerGroundCheck;
+    [SerializeField] private Transform ceilingCheck;
+    [SerializeField] private Transform leftWallCheck;
+    [SerializeField] private Transform rightWallCheck;
+    [SerializeField] private GameObject leftHitBox;
+    [SerializeField] private GameObject rightHitBox;
 
-    const float colliderCheckRadius = .1f;
+    [SerializeField] private float gravityScale = 3;
 
     [Range(0, 10)] [SerializeField] private float runSpeed;
     [Range(0, 0.5f)] [SerializeField] private float movementSmoothing = .1f;
     [Range(0, 10)] [SerializeField] private float climbSpeed;
     [Range(0, 800)] [SerializeField] private float jumpForce;
 
-    public Location location { get; set; }
-    private bool isFacingRight = true;
-    private Vector2 velocity = Vector2.zero;
+    const float colliderCheckRadius = .1f;
 
-    private Vector2 movementInput = Vector2.zero;
-    //private bool jumpPressed = false;
+    public enum GroundState
+    {
+        OnTheGround,
+        OnTheWall,
+        UnderTheShelf,
+        OverTheShelf,
+        InTheAir
+    }
+
+    public GroundState groundState { get; set; }
+
+    private bool isFacingRight = true;
+    private bool isCrouching = false;
+    private bool isAttacking = false;
+
+    private float xInput;
+    private float yInput;
+    private bool isHoldingCrouch;
+
+    private bool isJumpPressed = false;
+    private bool isAttackPressed;
+    private Vector2 velocity = Vector2.zero;
+    private LayerMask groundLayer;
+    private LayerMask wallLayer;
+    private LayerMask shelfLayer;
+    #endregion
+
 
     private void Awake()
     {
         rigidbody2D = GetComponent<Rigidbody2D>();
-        animator = body.GetComponent<Animator>();
+        animator = bodyTransform.GetComponent<Animator>();
     }
 
     private void Update()
     {
-        movementInput.x = InputController.Instance.GetHorizontalAxisRaw();
-        movementInput.y = InputController.Instance.GetVerticalAxisRaw();
+        xInput = Input.GetAxisRaw("Horizontal");
+        yInput = Input.GetAxisRaw("Vertical");
+        isHoldingCrouch = Input.GetAxisRaw("Horizontal") < 0 ? true : false;
+        isJumpPressed = Input.GetKeyDown(KeyCode.K);
+        isAttackPressed = Input.GetKeyDown(KeyCode.J);
 
-        //if (InputController.Instance.GetJumpKeyDown())
-        //{
-        //    jumpPressed = true;
-        //}
 
-        if (FeetTouchGround())
+        // Check state and facing direction.
+        if (IsCheckerTouchingLayer(lowerGroundCheck, groundLayer))
         {
-            location = Location.Ground;
-        }
-        else if (BodyTouchWall())
-        {
-            location = Location.Wall;
+            if (IsCheckerTouchingLayer(groundCheck, groundLayer))
+            {
+                groundState = GroundState.OnTheGround;
+            }
+            else
+            {
+                groundState = GroundState.InTheAir;
+            }
         }
         else
         {
-            location = Location.Air;
+            if(IsCheckerTouchingLayer(groundCheck, shelfLayer))
+            {
+                groundState = GroundState.OverTheShelf;
+            }
+            else if (IsCheckerTouchingLayer(ceilingCheck, shelfLayer))
+            {
+                groundState = GroundState.UnderTheShelf;
+            }
+            else
+            {
+                if (IsCheckerTouchingLayer(leftWallCheck, wallLayer))
+                {
+                    groundState = GroundState.OnTheWall;
+                    isFacingRight = false;
+                }
+                else if (IsCheckerTouchingLayer(rightWallCheck, wallLayer))
+                {
+                    groundState = GroundState.OnTheWall;
+                    isFacingRight = true;
+                }
+                else
+                {
+                    groundState = GroundState.InTheAir;
+                }
+            }
         }
 
+        // Check flip
+        CheckFlip();
+    }
 
-        if (location == Location.Ground)
+
+    private bool IsCheckerTouchingLayer(Transform checker, LayerMask layerMask)
+    {
+        return Physics2D.OverlapCircle(checker.position, colliderCheckRadius, layerMask);
+    }
+
+
+    private void CheckFlip()
+    {
+        if (groundState != GroundState.OnTheWall)
         {
-            wallCheckL.gameObject.SetActive(false);
-            wallCheckR.gameObject.SetActive(false);
-            rigidbody2D.gravityScale = 1;
-            SetAnimTrigger("idle");
-            CheckFlip();
-        }
-        else if (location == Location.Wall)
-        {
-            rigidbody2D.velocity = Vector2.zero;
-            rigidbody2D.gravityScale = 0;
-            SetAnimTrigger("onwall");
-        }
-        else if (location == Location.Air)
-        {
-            wallCheckL.gameObject.SetActive(true);
-            wallCheckR.gameObject.SetActive(true);
-            rigidbody2D.gravityScale = 1;
-            SetAnimTrigger("fall");
-            CheckFlip();
+            if (xInput > 0 && !isFacingRight || xInput < 0 && isFacingRight)
+            {
+                // Switch the way the player is labelled as facing.
+                isFacingRight = !isFacingRight;
+
+                // Multiply the player's x local scale by -1.
+                Vector3 theScale = bodyTransform.localScale;
+                theScale.x *= -1;
+                bodyTransform.localScale = theScale;
+            }
         }
     }
 
 
     private void FixedUpdate()
     {
+        // Check Gravity
+        CheckGravity();
+
+        // Check move
+        Move();
+
+        if(isHoldingCrouch)
+        {
+            CrouchHolding();
+        }
+        else
+        {
+            CrouchReleased();
+        }
+
+        if (isJumpPressed)
+        {
+            JumpPressed();
+        }
+
+        if (isAttackPressed)
+        {
+            AttackPressed();
+        }
+
+        isJumpPressed = false;
+    }
+
+
+    private void CheckGravity()
+    {
+        if (groundState == GroundState.InTheAir)
+        {
+            rigidbody2D.gravityScale = this.gravityScale;
+        }
+        else
+        {
+            rigidbody2D.gravityScale = 0;
+        }
+    }
+
+
+    private void Move()
+    {
         Vector2 move = Vector2.zero;
 
-        if (location == Location.Ground)
+        // Move...
+        if (groundState == GroundState.OnTheGround)
         {
-            if (InputController.Instance.GetAttackKeyDown())
+            if (!isCrouching && !isAttacking)
             {
-                if (InputController.Instance.GetVerticalAxisRaw() > 0)
-                {
-                    Ninjutsu();
-                    move = new Vector2(movementInput.x * runSpeed, 0);
-                }
-                else if (InputController.Instance.GetVerticalAxisRaw() < 0)
-                {
-                    NormalAttack();
-                }
-                else
-                {
-                    move = new Vector2(movementInput.x * runSpeed, 0);
-                }
+                move = new Vector2(xInput * runSpeed, 0);
             }
         }
-        else if (location == Location.Wall)
+        else if (groundState == GroundState.OnTheWall)
         {
-            if(InputController.Instance.GetAttackKeyDown())
+            if (!isAttacking)
             {
-                Ninjutsu();
-            }
-            else
-            {
-                move = new Vector2(0, movementInput.y * climbSpeed);
+                move = new Vector2(0, yInput * climbSpeed);
             }
         }
-        else if (location == Location.Air)
+        else if (groundState == GroundState.UnderTheShelf)
         {
-            if(InputController.Instance.GetAttackKeyDown())
+            if (!isAttacking)
             {
-                if(InputController.Instance.GetVerticalAxisRaw() > 0)
-                {
-                    Ninjutsu();
-                }
-                else
-                {
-                    NormalAttack();
-                }
+                move = new Vector2(xInput * runSpeed, 0);
             }
-            move = new Vector2(movementInput.x * runSpeed, rigidbody2D.velocity.y);
+        }
+        else if (groundState == GroundState.InTheAir)
+        {
+            move = new Vector2(xInput * runSpeed, rigidbody2D.velocity.y);
         }
 
         rigidbody2D.velocity = Vector2.SmoothDamp(rigidbody2D.velocity, move, ref velocity, movementSmoothing);
+    }
 
 
-        if (InputController.Instance.GetJumpKeyDown())
+    private void CrouchHolding()
+    {
+        if(groundState == GroundState.OnTheGround || groundState == GroundState.OverTheShelf)
         {
-            Vector2 force = Vector2.zero;
+            isCrouching = true;
+        }
+    }
 
-            if (location == Location.Ground || location == Location.Shelf)
+
+    private void CrouchReleased()
+    {
+        isCrouching = false;
+    }
+
+
+    private void JumpPressed()
+    {
+        Vector2 force = Vector2.zero;
+
+        if (groundState == GroundState.OnTheGround)
+        {
+            force = new Vector2(0, jumpForce);
+        }
+        else if (groundState == GroundState.OnTheWall)
+        {
+            force = new Vector2(jumpForce * (isFacingRight ? 1 : -1), jumpForce) / 2;
+        }
+        else if (groundState == GroundState.UnderTheShelf)
+        {
+            force = new Vector2(0, jumpForce);
+        }
+        else if (groundState == GroundState.OverTheShelf)
+        {
+            if (isCrouching)
             {
-                rigidbody2D.AddForce(new Vector2(0, jumpForce));
-                SetAnimTrigger("jump");
+                force = new Vector2(0, -jumpForce) / 2;
             }
-            else if (location == Location.Wall)
+            else
             {
-                if (isFacingRight)
-                {
-                    force = new Vector2(-jumpForce, jumpForce) / 2;
-                }
-                else
-                {
-                    force = new Vector2(jumpForce, jumpForce) / 2;
-                }
-                SetAnimTrigger("jump");
+                force = new Vector2(0, jumpForce);
             }
-
-            rigidbody2D.AddForce(force);
         }
-    }
 
-    private void Ninjutsu()
-    {
-        SetAnimTrigger("ninjutsu");
-    }
-
-    private void NormalAttack()
-    {
-        SetAnimTrigger("ninjutsu");
+        rigidbody2D.AddForce(force);
     }
 
 
-    private void CheckFlip()
+    private void AttackPressed()
     {
-        if (movementInput.x > 0 && !isFacingRight || movementInput.x < 0 && isFacingRight)
+        isAttacking = true;
+
+        if (Input.GetAxisRaw("Vertical") > 0)
         {
-            // Switch the way the player is labelled as facing.
-            isFacingRight = !isFacingRight;
-
-            // Multiply the player's x local scale by -1.
-            Vector3 theScale = body.localScale;
-            theScale.x *= -1;
-            body.localScale = theScale;
+            StartCoroutine(Ninjutsu());
         }
-    }
-
-
-    private bool FeetTouchGround()
-    {
-        if (groundCheck.gameObject.activeInHierarchy &&
-            Physics2D.OverlapCircle(groundCheck.position, colliderCheckRadius, GameController.groundLayer))
-            return true;
-        return false;
-    }
-
-    private bool LeftBodyTouchWall()
-    {
-        if (wallCheckL.gameObject.activeInHierarchy &&
-            Physics2D.OverlapCircle(wallCheckL.position, colliderCheckRadius, GameController.wallLayer))
-            return true;
-        return false;
-    }
-
-    private bool RightBodyTouchWall()
-    {
-        if (wallCheckR.gameObject.activeInHierarchy &&
-            Physics2D.OverlapCircle(wallCheckR.position, colliderCheckRadius, GameController.wallLayer))
-            return true;
-        return false;
-    }
-
-    private bool BodyTouchWall()
-    {
-        if (LeftBodyTouchWall() || RightBodyTouchWall())
-            return true;
-        return false;
-    }
-
-
-    private void SetAnimTrigger(string triggerName)
-    {
-        string[] triggers = GetAnimTriggers();
-        bool exist = false;
-
-        foreach (string trig in triggers)
-        {
-            if (trig == triggerName) exist = true;
-            animator.ResetTrigger(trig);
-        }
-
-        if (exist)
-            animator.SetTrigger(triggerName);
         else
-            throw new NotImplementedException();
+        {
+            StartCoroutine(NormalAttack());
+        }
     }
 
 
-    private string[] GetAnimTriggers()
+    private IEnumerator NormalAttack()
     {
-        return new string[]
+        if (isFacingRight)
         {
-            "idle",
-            "run",
-            "attack",
-            "crouch_idle",
-            "crouch_attack",
-            "ninjutsu",
-            "jump",
-            "jump_attack",
-            "jump_ninjutsu",
-            "fall",
-            "onwall_idle",
-            "onwall_move",
-            "onwall_ninjutsu",
-            "onshelf_idle",
-            "onshelf_move",
-            "onshelf_ninjutsu"
-        };
+            rightHitBox.SetActive(true);
+        }
+        else
+        {
+            leftHitBox.SetActive(true);
+        }
+
+        if (groundState == GroundState.OnTheGround)
+        {
+            if(isCrouching)
+            {
+
+            }
+            else
+            {
+
+            }
+        }
+        else if (groundState == GroundState.OnTheWall)
+        {
+
+        }
+        else if (groundState == GroundState.UnderTheShelf)
+        {
+
+        }
+        else if (groundState == GroundState.InTheAir)
+        {
+
+        }
+
+        yield return new WaitForSeconds(.2f);
+
+        leftHitBox.SetActive(false);
+        rightHitBox.SetActive(false);
+        isAttacking = false;
+    }
+
+
+    private IEnumerator Ninjutsu()
+    {
+        yield return new WaitForSeconds(.2f);
+        isAttacking = false;
     }
 }
